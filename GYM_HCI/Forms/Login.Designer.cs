342c137790c87b6bba2372f20d2f84cb01b82336
PiniginIA <PiniginIA@313D-10>
2022-04-28 12:15:52 +0700

    1

---
+++ GYM_HCI/Forms/Login.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace GYM_HCI.Forms
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.cbTypeOfUser = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Location = new System.Drawing.Point(290, 147);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(140, 20);
+            this.tbLogin.TabIndex = 0;
+            // 
+            // tbPass
+            // 
+            this.tbPass.Location = new System.Drawing.Point(290, 182);
+            this.tbPass.Name = "tbPass";
+            this.tbPass.Size = new System.Drawing.Size(140, 20);
+            this.tbPass.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(323, 261);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "войти";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // cbTypeOfUser
+            // 
+            this.cbTypeOfUser.FormattingEnabled = true;
+            this.cbTypeOfUser.Items.AddRange(new object[] {
+            "Клиент",
+            "Тренер",
+            "Администратор"});
+            this.cbTypeOfUser.Location = new System.Drawing.Point(290, 219);
+            this.cbTypeOfUser.Name = "cbTypeOfUser";
+            this.cbTypeOfUser.Size = new System.Drawing.Size(140, 21);
+            this.cbTypeOfUser.TabIndex = 3;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbTypeOfUser);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.tbPass);
+            this.Controls.Add(this.tbLogin);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.ComboBox cbTypeOfUser;
+    }
+}
---
+++ GYM_HCI/Forms/Login.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GYM_HCI.Forms
+{
+    public partial class Login : Form
+    {
+        string strConn = Classes.StrConnection.strConn;
+        SqlConnection connection = new SqlConnection(strConn);
+
+        public int LoginUser()
+        {
+            int TypeOfUser = cbTypeOfUser.SelectedIndex;
+            int newProdID = 0;
+            string sql = "select count(*)from [Данные авторизации] where [Логин] = '" + tbLogin.Text + "' and [Пароль] = '" + tbPass.Text + "'and [Тип пользователя] = '" + TypeOfUser + "'";
+            using (SqlConnection conn = new SqlConnection(strConn))
+
+            {
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                try
+                {
+                    conn.Open();
+                    newProdID = (int)cmd.ExecuteScalar();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            
+            //MessageBox.Show(newProdID.ToString());
+            return (int)newProdID;
+        }
+        public void logins()
+        {
+            
+            string sqlCommand = "select [Администратор] from [Данные авторизации] where [Логин] = '" + textBox1.Text + "' and [Пароль] = '" + textBox2.Text + "'";
+            using (SqlConnection connection = new SqlConnection(strConn));
+            SqlCommand command = new SqlCommand(sqlCommand,     connection);
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows) // если есть данные
+            {
+
+                while (reader.Read())
+                {
+                    if (reader.GetBoolean(0) == true)
+                        a = 1;
+                }
+            }
+            int c = countLogins() + a;
+            if (c == 1)
+            {
+                loginkrutoi228 = textBox1.Text;
+                Lichka newForm = new Lichka();
+                newForm.Show();
+                this.Close();
+
+            }
+            else
+                if (c == 2)
+            {
+                loginkrutoi228 = textBox1.Text;
+                LichkaAdmin newForm = new LichkaAdmin();
+                newForm.Show();
+                this.Close();
+            }
+        }
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
